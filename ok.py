c271b65d86703cdbad2d1fc16bf55db6811803f2
Lavanya411 <60595160+Lavanya411@users.noreply.github.com>
2020-02-27 15:44:44 +0530

    Add files via upload

---
+++ ok.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[21]:
+
+
+class dog:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def sit(self):
+        print(f"my dog name is {self.name}")
+    def roll_over(self):
+        print(f"{self.name} is roll over")
+    
+    
+
+
+# In[ ]:
+
+
+
+
